e7a69cde3b903942d60066a98fc6eff5d3246089
Nikita2341 <sokulnikita71@gmail.com>
2022-01-19 12:55:48 +0300

    корректировка структуры проекта

---
+++ MySolution/DuckLibrary/Duck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DuckPool
+{
+    public abstract class Duck
+    {
+        public string Quack()
+        {
+            return $"Quack {GetType()}";
+        }
+
+        public string Swim()
+        {
+            return $"Swim {GetType()}";
+        }
+
+        abstract public string Display();
+    }
+}
---
+++ MySolution/MySolution/Program.cs
@@ -0,0 +1,29 @@
+﻿using DuckPool;
+using System;
+using System.Collections.Generic;
+
+namespace bruhDuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck mDuck = new MallardDuck();
+            Duck rDuck = new RedheadDuck();
+
+            List<Duck> ducks = new List<Duck>()
+            {
+                mDuck,
+                rDuck
+            };
+
+            ducks.ForEach(duck => {
+                Console.WriteLine(duck.Quack());
+                Console.WriteLine(duck.Swim());
+                Console.WriteLine(duck.Display());
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
